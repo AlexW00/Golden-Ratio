ec8ad759c08fc260a48b10f6ee85dab4fbaceac8
Alexander Weichart <55558407+AlexW00@users.noreply.github.com>
2026-07-12 18:51:08 +0900

    Initial Commit

---
+++ Golden Ratio/Golden_RatioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Golden_RatioApp.swift
+//  Golden Ratio
+//
+//  Created by Alex Weichart on 12.07.26.
+//
+
+import SwiftUI
+
+@main
+struct Golden_RatioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Golden RatioTests/Golden_RatioTests.swift
@@ -0,0 +1,19 @@
+//
+//  Golden_RatioTests.swift
+//  Golden RatioTests
+//
+//  Created by Alex Weichart on 12.07.26.
+//
+
+import Testing
+@testable import Golden_Ratio
+
+struct Golden_RatioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
